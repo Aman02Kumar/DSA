81d516ff939d3725a7607b141cdb181c528605b7
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2024-11-07 21:29:39 +0530

    Time: 5 ms (68.24%) | Memory: 14.7 MB (12.97%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,21 +1,74 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& arr) {
-        
-        long long sum = 0 ;
-        
-        for(int i = 0 ; i < arr.size();i++){
+    long long subArrayRanges(vector<int>& nums) {
+        return sumMax(nums) - sumMin(nums);
+    }
 
-            int lar = arr[i] , smal = arr[i];
-            for(int j = i+1 ; j < arr.size();j++){
-
-                    lar = max(arr[j],lar);
-                    smal = min (arr[j] , smal);
-
-                    sum = sum + (lar - smal);
-
-            }
+    long long sumMin(vector<int>& arr){
+        int n = arr.size();
+        long long total = 0;
+        vector<int> nse = findNse(arr, n), pse = findPse(arr, n);
+        for(int i = 0; i < n; i++){
+            long long left = i - pse[i];
+            long long right = nse[i] - i;
+            total += right * left * arr[i];
         }
-        return sum;
+        return total;
+    }
+
+    vector<int> findNse(vector<int>& arr, int n){
+        vector<int> nse(n);
+        stack<int> st;
+        for(int i = n-1; i >= 0; --i){
+            while(!st.empty() && arr[i] < arr[st.top()]) st.pop();
+            nse[i] = st.empty()? n: st.top();
+            st.push(i);
+        }
+        return nse;
+    }
+
+    vector<int> findPse(vector<int>& arr, int n){
+        vector<int> pse(n);
+        stack<int> st;
+        for(int i = 0; i < n; ++i){
+            while(!st.empty() && arr[i] <= arr[st.top()]) st.pop();
+            pse[i] = st.empty()? -1: st.top();
+            st.push(i);
+        }
+        return pse;
+    }
+
+    long long sumMax(vector<int>& arr){
+        int n = arr.size();
+        vector<int> nge = findNge(arr, n), pge = findPge(arr, n);
+        long long total = 0;
+        for(int i = 0; i < n; i++){
+            long long left = i - pge[i];
+            long long right = nge[i] - i;
+            total += right * left * arr[i];
+        }
+        return total;
+    }
+
+    vector<int> findNge(vector<int>& arr, int n){
+        vector<int> nge(n);
+        stack<int> st;
+        for(int i = n-1; i >= 0; --i){
+            while(!st.empty() && arr[i] > arr[st.top()]) st.pop();
+            nge[i] = st.empty()? n: st.top();
+            st.push(i);
+        }
+        return nge;
+    }
+
+    vector<int> findPge(vector<int>& arr, int n){
+        vector<int> pge(n);
+        stack<int> st;
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && arr[i] >= arr[st.top()]) st.pop();
+            pge[i] = st.empty()? -1: st.top();
+            st.push(i);
+        }
+        return pge;
     }
 };
