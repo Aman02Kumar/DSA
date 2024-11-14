a86357abc0e4249b718893f37ef4c0564245fc4b
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2024-11-14 23:06:07 +0530

    Time: 39 ms (46.49%) | Memory: 86.3 MB (25.96%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,50 +1,55 @@
 class Solution {
 public:
-    vector<int> nse(vector<int>& arr) {
-        vector<int> ans(arr.size(), arr.size());  // Set default value to arr.size() for no NSE on the right
-        stack<int> st;
 
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
+    vector<int> nse(vector<int>& arr){
+
+            vector<int> ans(arr.size(),arr.size());
+            stack<int> st;
+
+            for(int i = arr.size()-1; i >= 0 ;i--){
+
+                while(!st.empty() && arr[st.top()] >= arr[i]){
+                    st.pop();
+                }
+
+                if(!st.empty()  ){
+                    ans[i] = st.top();
+                }
+
+                st.push(i);
             }
-
-            if (!st.empty()) {
-                ans[i] = st.top();
-            }
-
-            st.push(i);  // Push index, not value
-        }
-        return ans;
+            return ans;
     }
+    
 
-    vector<int> pse(vector<int>& arr) {
-        vector<int> ans2(arr.size(), -1);  // Set default value to -1 for no PSE on the left
-        stack<int> st;
+    vector<int> pse(vector<int>& arr){
 
-        for (int i = 0; i < arr.size(); i++) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
+            vector<int> ans2(arr.size(),-1);
+            stack<int> st;
+
+            for(int i =0; i < arr.size() ;i++){
+
+                while(!st.empty() && arr[st.top()] > arr[i]){
+                    st.pop();
+                }
+
+                if(!st.empty()  ){
+                    ans2[i] = st.top();
+                }
+
+                st.push(i);
             }
-
-            if (!st.empty()) {
-                ans2[i] = st.top();
-            }
-
-            st.push(i);  // Push index, not value
-        }
-        return ans2;
+            return ans2;
     }
-
     int largestRectangleArea(vector<int>& arr) {
-        int maxi = 0;
-        vector<int> right = nse(arr);  // NSE values
-        vector<int> left = pse(arr);   // PSE values
 
-        for (int i = 0; i < arr.size(); i++) {
-            int width = right[i] - left[i] - 1;
-            maxi = max(maxi, arr[i] * width);
-        }
-        return maxi;
+            int maxi = 0 ;
+            vector<int> right = nse(arr);
+            vector<int> left = pse(arr);
+
+            for(int i = 0 ; i < arr.size() ; i++){
+                    maxi =max(maxi , arr[i]*(right[i] - left[i] - 1 ));
+            }
+            return maxi;
     }
 };
