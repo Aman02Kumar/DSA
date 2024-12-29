4f900f51ac652518037a255cd610e7cfa8c8275f
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2024-12-29 23:02:13 +0530

    Time: 2 ms (72.25%) | Memory: 11.6 MB (34.12%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,39 +1,53 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
+        int n = num.size();
+        stack<char> s;
+
+
+    //we iterate over each element of string and run while loop for every element and pop the stack if while condition is true
+    //if while is false -> we simply push the num[i] into the stack
+
+    for(int i = 0; i<n; i++){
+        while( !s.empty() && k > 0 && ( s.top() - '0' ) > ( num[i] - '0' ) ){
+            s.pop();
+            k--;
+        }
+        s.push(num[i]);
+    }
+
+    //handling edge case 3 -> if we have a incremental string then no pop occurs as every num[i] > s.top() -> so we might end up adding all elements to stack and k didn't decrease at all, or we might removed some elements but k still isn't zero that means we reached incremental string state midway and some part of the string is incremental rather entire string being incremental -> so we handle this case by simply popping the stack equal to remaining k.
+    
+    while( k > 0 ){
+        s.pop();
+        k--;
+    }
+
+
+    //At this point out stack has the result in reversed order.
+
+    string result = "";
+    while( !s.empty() ){
         
-        stack<char> st;
-        int  n = num.size();
-      
-        for(int i = 0 ;i < num.size() ; i++){
+        //storing the result is reverse order in string
+        result += s.top();
+        s.pop();
+    }
 
-            while(!st.empty()  &&  k > 0 && (st.top() - '0' > num[i] -'0')){
-                st.pop();
-                k--;
-            }
-            st.push(num[i]);
-        }
-        while(k > 0 && !st.empty())     k--, st.pop();
+    //handling 2nd edge cases -> to eliminate leading zeros, we check if the back which is effectively the start of our answer is zero or not and unti result is not empty and string starts with zero, we keep removing it, as soon as all starting zeros are removed or string gets empty the result contains the answer in reverse order
 
-        if(st.empty()) return "0";
+    while( !result.empty() && result.back() == '0'){
+        result.pop_back();
+    }
 
-        string res = "";
-        while(!st.empty()){
-            res += st.top();
-            st.pop();
-        }
+    reverse(result.begin(), result.end());
 
-        reverse(res.begin() , res.end() );
 
-        // removing zeroes from anser
-        int index = 0 ;
-        while(index < res.size()  && res[index] =='0'){
-            index++;
+    //handling 1st edge case -> k == n where we end up removing everything or we all element in result were zeros 
+    if(result.empty()){
+        return "0";
+    }
 
-        }
-
-        res = res.substr(index);
-        return res.empty() ? "0" :res;
-
+    return result;
     }
 };
