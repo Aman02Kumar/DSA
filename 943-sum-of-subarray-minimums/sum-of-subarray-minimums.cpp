ddbdc355b4f5cf8b93cf4bc148c5321d4bbeb15f
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2024-12-29 20:24:46 +0530

    Time: 23 ms (41.23%) | Memory: 47.3 MB (31.34%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
-    vector<int> nextSmallerElementRight(vector<int>& nums) {
-        vector<int> ans;
-        stack<int> st;
+    vector<int> findnse(vector<int> &arr){
+        int n = arr.size();
+        vector<int> nse(n);
+        stack<int> st1;
 
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] >= nums[i])
-                st.pop();
-            if (st.empty())
-                ans.push_back(nums.size());
-            else
-                ans.push_back(st.top());
-            st.push(i);
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!st1.empty() && arr[st1.top()] >= arr[i])
+                st1.pop();
+            nse[i] = st1.empty() ? n : st1.top();
+            st1.push(i);    
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        return nse;
+
     }
+    vector<int> findpsee(vector<int> &arr){
+        int n = arr.size();
+        vector<int> pse(n);
+        stack<int> st2;
 
-    vector<int> nextSmallerElementLeft(vector<int>& nums) {
-        vector<int> ans;
-        stack<int> st;
-
-        for (int i = 0; i < nums.size(); i++) {
-            while (!st.empty() && nums[st.top()] > nums[i])
-                st.pop();
-            if (st.empty())
-                ans.push_back(-1);
-            else
-                ans.push_back(st.top());
-
-            st.push(i);
+        for(int i = 0 ; i <n ; i++){
+            while(!st2.empty() && arr[st2.top()] > arr[i])
+                st2.pop();
+            pse[i] = st2.empty() ? -1 : st2.top();
+            st2.push(i);    
         }
-        return ans;
+        return pse;
     }
-
     int sumSubarrayMins(vector<int>& arr) {
-        vector<int> left = nextSmallerElementLeft(arr);
-        vector<int> right = nextSmallerElementRight(arr);
 
-        long long int count = 0;
-        long long mod = 1e9 +7;
-        for (int i = 0; i < arr.size(); i++) {
-            cout<<left[i]<<" "<<right[i]<<"  ";
-            count += (abs(left[i] - i)%mod * abs(right[i] - i)%mod * arr[i]%mod)%mod;
+        vector<int> nse = findnse(arr);
+        vector<int> psee = findpsee(arr);
+        int total = 0; 
+        int mod = (int)(1e9 + 7);
+        for(int i = 0 ; i < arr.size() ; i++){
+            int left = i - psee[i];
+            int right = nse[i] - i;
+
+            total = (total + (right*left* 1LL * arr[i] )%mod)%mod ;
+
+
         }
-        return count%mod;
+        return total;
     }
 };
