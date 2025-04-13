176f34dece5a5f79444f29c5b546d0a7b37ea73b
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-04-13 20:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (73.55%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -2,34 +2,26 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int n = chars.size();
-
         int index = 0 ; 
         int i = 0 ;
-
         while(i<n){
-
             int cnt = 0;
             char cc = chars[i];
-
             // finding count of duplicatess
             while(i < n && chars[i] == cc){
                 cnt++;
                 i++;
             }
-
             // now do the asssign work
             chars[index] = cc;
             index++;
-
             if(cnt > 1 ){
-
                 string str = to_string(cnt);
                 for(auto it : str){
                     chars[index] = it;
                     index++;
                 }
             }
-
         }
         return index;
     }
