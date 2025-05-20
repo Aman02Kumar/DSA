a9f41ac7ed408845251ddb12e5e354a266b78380
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-05-20 16:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (82.22%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isLetter(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+
+    string reverseOnlyLetters(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            if (!isLetter(s[left])) {
+                left++;
+            } else if (!isLetter(s[right])) {
+                right--;
+            } else {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
