371ea03717e3c1eba028635b124fd1e694239607
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-05-26 20:36:12 +0530

    Time: 1 ms (92.68%) | Memory: 71.4 MB (69.44%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-       unordered_map<int, int> mp;
+        int n = nums.size();
 
-        int n       = nums.size();
-        int count   = 0;
-        int currSum = 0;
-        mp[currSum] = 1;
+        int oddCount = 0;
+        int count = 0;
+        int result = 0;
 
-        for(int i = 0; i < n; i++) {
-            currSum += (nums[i]%2); //if odd - 1, even - 0
+        int i = 0;
+        int j = 0;
+        // Best example to understand the use of count variable - {2, 1, 2, 1},
+        // output : 6
+        while (j < n) {
 
-            if(mp.count(currSum - k)) {
-                count += mp[currSum-k];
+            if (nums[j] % 2 != 0) { // ODD
+                oddCount++;
+                count = 0; // We need to reset this to avoid over counting.
+                           // Example : [1,1,2,1,1], k = 3
             }
 
-            mp[currSum]++;
+            while (oddCount >= k) {
+                count++;
+
+                if (i < n && nums[i] % 2 == 1) { // ODD
+                    oddCount--;
+                }
+                i++;
+            }
+
+            result += count;
+            j++;
         }
 
-        return count;
-
+        return result;
     }
 };
-
