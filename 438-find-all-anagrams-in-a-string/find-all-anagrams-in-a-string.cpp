b6cd2708b724896055ec6660c17d53426079ed1d
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-05-26 16:55:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (96.95%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-         int sLen = s.length(), pLen = p.length();
-    if (sLen < pLen) return {};
-
-    vector<int> pCount(26, 0), sCount(26, 0), result;
 
 
-    for (char c : p) pCount[c - 'a']++;
 
-    for (int i = 0; i < pLen; i++) sCount[s[i] - 'a']++;
+vector<int> findAnagrams(string s, string p) {
+    vector<int> result;
+    if (s.length() < p.length())
+        return result;
 
+    vector<int> p_count(26, 0), s_count(26, 0);
 
-    if (pCount == sCount) result.push_back(0);
-    for (int i = pLen; i < sLen; i++) {
-        sCount[s[i] - 'a']++;           
-        sCount[s[i - pLen] - 'a']--;      
-        if (pCount == sCount) result.push_back(i - pLen + 1);
+    // Count frequency of characters in p
+    for (char c : p)
+        p_count[c - 'a']++;
+
+    int windowSize = p.length();
+
+    // First window
+    for (int i = 0; i < windowSize; ++i)
+        s_count[s[i] - 'a']++;
+
+    if (s_count == p_count)
+        result.push_back(0);
+
+    // Slide the window
+    for (int i = windowSize; i < s.length(); ++i) {
+        // Add new character
+        s_count[s[i] - 'a']++;
+        // Remove the character going out of the window
+        s_count[s[i - windowSize] - 'a']--;
+
+        if (s_count == p_count)
+            result.push_back(i - windowSize + 1);
     }
 
     return result;
-    }
-};
+}
+}
+;
