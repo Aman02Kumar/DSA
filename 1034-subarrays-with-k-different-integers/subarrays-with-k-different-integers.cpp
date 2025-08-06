41d36bf204de70ddc6d3e1a5a93170ef733d4bc9
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-08-06 14:02:43 +0530

    Time: 163 ms (12.30%) | Memory: 67.3 MB (9.95%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+     int atmost(vector<int>& nums,int k){
+
+        int i=0;
+        int j=0;
+        int count =0;
+        int n = nums.size();
+        int sum =0;
+        map<int,int> m;
+
+        while( j < n){
+            m[nums[j]]++;
+
+            if(m.size() > k){
+                while(m.size() > k){
+                    m[nums[i]]--;
+                    if(m[nums[i]] == 0){
+                        m.erase(nums[i]);
+                    }
+                    i++;
+                }
+            }
+            count = count + j-i+1;
+            j++;
+        }
+
+        return count;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int x) {
+
+        return atmost(nums,x)-atmost(nums,x-1);
+
+    }
+};
