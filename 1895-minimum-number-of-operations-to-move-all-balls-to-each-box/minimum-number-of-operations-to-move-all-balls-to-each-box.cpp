7dcbfd487f238342a7fd65479517f0bb8ad471bf
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-08-23 19:07:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (68.08%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int n=boxes.size();
-        int sum=0;
-        int right_ones=0;
-        for(int i=0;i<n;++i)
-            if(boxes[i]=='1'){
-                sum += i;
-                right_ones++;
-            }
-        
-        //Now calculate moves per position
-        vector<int> res(n);
-        int prefix_sum=0;
-        int left_ones=0;
-        for(int i=0;i<n;++i){
-            int moves = sum + prefix_sum;
-            res[i]=moves;
+        int n = boxes.size();
 
-            //Update both sum and prefix_sum
-            if(boxes[i]=='1'){
-                left_ones++;
-                right_ones--;
-            }
-            prefix_sum += left_ones;
-            sum -= right_ones;
+        vector<int> answer(n, 0);
+
+        int cumValue    = 0;
+        int cumValueSum = 0;
+
+        //Find moves for all left balls to index i
+        //O(n)
+        for(int i = 0; i < n; i++) {
+            answer[i] = cumValueSum;
+            
+            cumValue += boxes[i] == '0' ? 0 : 1;
+            
+            cumValueSum += cumValue;
         }
-        return res;
+
+        cumValue    = 0;
+        cumValueSum = 0;
+        //Find moves for all right balls to index i
+        //O(n)
+        for(int i = n-1; i >= 0; i--) {
+            answer[i] += cumValueSum;
+            
+            cumValue += boxes[i] == '0' ? 0 : 1;
+            
+            cumValueSum += cumValue;
+        }
+
+        //O(n+n) ~ O(n)
+        return answer;
+
     }
 };
