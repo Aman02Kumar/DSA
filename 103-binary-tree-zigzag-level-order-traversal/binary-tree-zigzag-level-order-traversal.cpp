4dd1af63cb617562f29955493fca9060b06afb2c
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-08-30 04:49:00 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (47.92%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,42 +12,34 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
-        vector<vector<int>> ans;
-        if (root == nullptr) {
-            return ans;
+
+    vector<vector<int>> result;
+    if(!root) return result;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    bool leftToRight = true;
+
+    while(!q.empty()) {
+        int size = q.size();
+        vector<int> level(size);
+
+        for(int i=0; i<size; i++) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            int index = leftToRight ? i : (size-1-i);
+            level[index] = node->val;
+
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
         }
-        stack<TreeNode*> q1;
-        stack<TreeNode *> q2;
 
-        q1.push(root);
-        while(!q1.empty() || !q2.empty()){
-            vector<int> k;
-           
-            while(!q1.empty()){
-                    TreeNode * x = q1.top();
-                    q1.pop();
-                    k.push_back(x->val); 
-                    if(x->left) q2.push(x->left);
-                    if(x->right) q2.push(x->right);
-                    
-            }
-            
-            if(!k.empty()) ans.push_back(k);
-            k.clear(); 
-
-            while(!q2.empty()){
-                    TreeNode* x = q2.top();
-                    q2.pop();
-                    k.push_back(x->val); 
-                    if(x->right) q1.push(x->right);
-                    if(x->left) q1.push(x->left);
-                    
-                }
-            
-            if(!k.empty()) ans.push_back(k);
-
-        }
-        return ans;
+        result.push_back(level);
+        leftToRight = !leftToRight;
     }
+    return result;
+}
+
+
 };
