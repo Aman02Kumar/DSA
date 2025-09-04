1cc96b3ec545a938dabed1975293cc8ffd70ae57
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-09-05 05:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (6.69%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
         
+        unordered_set<int> st;
         int cnt = 0;
         for(int i = 0 ; i < nums.size();i++){
-            for(int j = i+1 ; j< nums.size();j++){
-                for(int k = j+1 ;k <nums.size() ; k++){
-                    if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
-                        cnt++;
-                    }
-                }
+            if(st.find(nums[i] - diff)  != st.end()  && st.find(nums[i] - 2*diff)  != st.end()){
+                cnt++;
             }
+            st.insert(nums[i]);
         }
         return cnt;
     }
