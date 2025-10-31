c455d5235e86e02c8368959caf790ad77effa5bb
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-10-31 10:51:09 +0530

    Time: 7 ms (9.16%) | Memory: 26.4 MB (34.01%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    vector<int> getSneakyNumbers(vector<int>& arr) {
+    vector<int> getSneakyNumbers(vector<int>& nums) {
         
-        map<int,int>mp;
-        int n = arr.size();
+        map<int,int> mp ;
         vector<int>ans;
-
-        for(int i = 0 ; i<n;i++){
-            if(mp[arr[i]] == 1) ans.push_back(arr[i]);
-            mp[arr[i]]++;
-            // if(mp[arr[i] == 1]) ans.push_back(arr[i])
+        for(auto i : nums){
+            if(mp.find(i) != mp.end()){
+                ans.push_back(i);
+            }
+            mp[i]++;
         }
         return ans;
     }
