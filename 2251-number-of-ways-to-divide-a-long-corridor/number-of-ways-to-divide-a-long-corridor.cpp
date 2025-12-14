a22fe4c561401d151c8c916585c195f7d2e95b05
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2025-12-14 15:47:31 +0530

    Time: 36 ms (33.50%) | Memory: 58.6 MB (40.00%) - LeetSync

---
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfWays(string corridor) {
+        const int MOD = 1e9 + 7;
+        vector<int> seats;
+
+        for (int i = 0; i < corridor.size(); i++) {
+            if (corridor[i] == 'S') {
+                seats.push_back(i);
+            }
+        }
+
+        int total = seats.size();
+        if (total == 0 || total % 2 != 0) return 0;
+
+        long long ways = 1;
+
+        for (int i = 2; i < total; i += 2) {
+            ways = (ways * (seats[i] - seats[i - 1])) % MOD;
+        }
+
+        return ways;
+    }
+};
