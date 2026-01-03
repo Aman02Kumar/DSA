52f39c9c904a46e6de1c1982b172f57af3c9e218
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2026-01-03 15:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.00%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numOfWays(int n) {
         const long long MOD = 1000000007;
-        long long A = 0, B = 1, C = 1, D = 0;
+        long long A =0, B = 1, C = 1, D = 0;
         for (int x = n - 1; x > 0; x >>= 1) {
             if (x & 1) {
                 long long nextA = (A*D + B*C + 5*A*C) % MOD;
