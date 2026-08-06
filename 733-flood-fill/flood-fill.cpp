457e4b9bc3feaf7c9aee7864289f1b245d139b02
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2026-08-06 23:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (14.07%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int newColor, vector<vector<int>>& ans, vector<vector<int>>& visit, int color) {
-        ans[sr][sc] = newColor;
-        visit[sr][sc] = 1;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
+        int n = image.size();
+        int m = image[0].size();
 
-        // Direction vectors for up, right, down, left
-        int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, 1, 0, -1};
+        vector<vector<int>> vis(n,vector<int>(m,0));
 
-        int rows = image.size();
-        int cols = image[0].size();
+        queue<pair<int,int>> q ;
+        q.push({sr,sc});
+        int old = image[sr][sc];
+        image[sr][sc]=color;
+        vis[sr][sc]=1;
 
-        for (int i = 0; i < 4; i++) {
-            int nr = sr + dr[i];
-            int nc = sc + dc[i];
 
-            // Check for valid cell and the color condition
-            if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && !visit[nr][nc] && image[nr][nc] == color) {
-                dfs(image, nr, nc, newColor, ans, visit, color);
+        while(!q.empty()){
+
+            int r = q.front().first;
+            int c = q.front().second;
+
+            int dr[] = {1,0,-1,0};
+            int dc[] = {0,1,0,-1};
+            q.pop();
+
+            for(int i = 0 ; i < 4 ; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if(nr >= 0 && nc >= 0 && nr < n && nc <m &&  vis[nr][nc]==0 && image[nr][nc]==old){
+
+                    image[nr][nc]=color;
+                    vis[nr][nc]= 1;
+                    q.push({nr,nc});
+                }
             }
         }
-    }
-
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int rows = image.size();
-        int cols = image[0].size();
-
-        // Initialize ans and visit arrays
-        vector<vector<int>> ans = image;
-        vector<vector<int>> visit(rows, vector<int>(cols, 0));
-        int color = image[sr][sc];
-
-        // If the starting pixel already has the new color, return the image
-        // if (color == newColor) {
-        //     return image;
-        // }
-
-        dfs(image, sr, sc, newColor, ans, visit, color);
-        return ans;
+        return image;
     }
 };
