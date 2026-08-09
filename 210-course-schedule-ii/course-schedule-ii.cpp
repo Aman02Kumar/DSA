e4b70599dfe1d73b4adb9b99faf61cb360d972fa
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2026-08-09 19:11:50 +0530

    Time: 6 ms (37.62%) | Memory: 18.7 MB (37.23%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,44 +1,65 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        vector<int> indegree(numCourses, 0);
-        int count = 0;
-        
-        // Build the adjacency list and calculate indegree
-        for (int i = 0; i < prerequisites.size(); i++) {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
-        }
-        
-        queue<int> q;
-        // Add all nodes with indegree 0 to the queue
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
+
+    bool dfs(int i,
+             vector<int>& vis,
+             vector<int> adj[],
+             stack<int>& st,
+             vector<int>& path) {
+
+        vis[i] = 1;
+        path[i] = 1;
+
+        for(auto it : adj[i]) {
+
+            if(!vis[it]) {
+
+                if(dfs(it, vis, adj, st, path)) {
+                    return true;
+                }
+            }
+            else if(path[it]) {
+
+                return true;
             }
         }
-        
-        vector<int> ans;
-        while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            ans.push_back(curr);
-            count++;
-            
-            for (int i = 0; i < adj[curr].size(); i++) {
-                indegree[adj[curr][i]]--;
-                if (indegree[adj[curr][i]] == 0) {
-                    q.push(adj[curr][i]);
+
+        path[i] = 0;
+        st.push(i);
+
+        return false;
+    }
+
+    vector<int> findOrder(int v, vector<vector<int>>& pre) {
+
+        vector<int> adj[v];
+
+        for(auto i : pre) {
+            adj[i[1]].push_back(i[0]);
+        }
+
+        vector<int> vis(v, 0);
+        vector<int> path(v, 0);
+
+        stack<int> st;
+
+        for(int i = 0; i < v; i++) {
+
+            if(!vis[i]) {
+
+                if(dfs(i, vis, adj, st, path)) {
+                    return {};
                 }
             }
         }
-        
-        // If all courses are not completed, return an empty array
-        if (count == numCourses) {
-            return ans;
-        } else {
-            return {};
+
+        vector<int> ans;
+
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
         }
+
+        return ans;
     }
 };
