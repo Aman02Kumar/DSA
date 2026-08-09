fcd963c459748e9a0937a120fb98440ad9d1e4f8
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2026-08-09 19:32:33 +0530

    Time: 60 ms (13.87%) | Memory: 63.5 MB (58.82%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        // reverse the graph and using topo sort;
-        int n = graph.size();
-        vector<int> indegree(n, 0);
-        vector<int> adjRev[n];
+        int n =graph.size();
+        vector<int> adj[n];
+        vector<int> in(n,0);
 
-        for (int i = 0; i < n; i++) {
-            for (auto it : graph[i]) {
-                adjRev[it].push_back(i);
-                indegree[i]++;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < graph[i].size();j++){
+                adj[graph[i][j]].push_back(i);
+                in[i]++;
             }
         }
 
-        vector<int> safeNodes;
         queue<int> q;
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0)
+        for(int i = 0 ; i < n; i++){
+            if(in[i]==0){
                 q.push(i);
+            }
+
         }
-
-        while (!q.empty()) {
-            int node = q.front();
+        vector<int> ans;
+        while(!q.empty()){
+            int n = q.front();
+            ans.push_back(n);
             q.pop();
-            safeNodes.push_back(node);
 
-            for (auto it : adjRev[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0)
+            for(auto it : adj[n]){
+                in[it]--;
+                if(in[it]==0){
                     q.push(it);
+                }
             }
         }
-
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
