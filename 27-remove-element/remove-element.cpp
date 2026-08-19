2ea439a22f2b8d42485c724276f06db1f47e548b
Aman Kaushik <119118234+Aman02Kumar@users.noreply.github.com>
2026-08-19 22:08:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.63%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
+    int removeElement(vector<int>& nums, int v) {
         int i = 0;
-        for (int j = 0; j < nums.size(); ++j) {
-            if (nums[j] != val) {
-                nums[i] = nums[j];
+        int j = nums.size() - 1;
+
+        while (i <= j) {
+
+            // Find v from left
+            while (i <= j && nums[i] != v) {
                 i++;
             }
+
+            // Find non-v from right
+            while (i <= j && nums[j] == v) {
+                j--;
+            }
+
+            if (i < j) {
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
         }
+
         return i;
     }
 };
